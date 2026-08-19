774af8ac1c5e6ec553d59f75ecfc2c296d1ad7d4
Censera <Censera@proton.me>
2026-08-19 10:59:29 +0100

    allow smaller JVM for backup validation

--- start.sh
+++ start.sh
@@ -3,12 +3,14 @@ set -e
 
 ROOT="${MINECERA_ROOT:-$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)}"
 JAR="${MINECERA_JAR:-$ROOT/paper-26.2.jar}"
+XMS="${MINECERA_XMS:-6G}"
+XMX="${MINECERA_XMX:-6G}"
 
 cd "$ROOT"
 
 exec java \
-    -Xms6G \
-    -Xmx6G \
+    -Xms"$XMS" \
+    -Xmx"$XMX" \
     -XX:+UseG1GC \
     -XX:+ParallelRefProcEnabled \
     -XX:MaxGCPauseMillis=200 \
